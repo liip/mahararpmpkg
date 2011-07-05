e9c4c666a1f10415c7aa23a2a048efe42335bcc2
Adrian Schlegel <adrian@liip.ch>
2011-07-05 23:01:27 +0200

    fixed git archive script

--- git-arch.sh
+++ git-arch.sh
@@ -2,7 +2,8 @@
 TMP=$1
 NV=$2
 
+export NV TMP
 echo "NV: $NV / TMP: $TMP"
-# git archive --format=tar --prefix="${NV}" --output=${TMP}/${NV}.tar HEAD
-# cd `git rev-parse --show-cdup`
-# git submodule --quiet foreach 'FNAME=`mktemp -p /tmp`;git archive --format=tar --output=$FNAME --prefix=${path}/ $sha1;tar -Af ${TMP}/${NV}.tar $FNAME;rm $FNAME'
+git archive --format=tar --prefix="${NV}" --output="${TMP}/${NV}.tar" HEAD
+cd `git rev-parse --show-cdup`
+git submodule --quiet foreach 'FNAME=`mktemp -p /tmp`;git archive --format=tar --output=$FNAME --prefix=${path}/ $sha1;tar -Af "${TMP}/${NV}.tar" $FNAME;rm $FNAME'
