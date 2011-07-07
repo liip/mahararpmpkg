c560e0f23d52197b76ba8d41080a5eecda527b05
Adrian Schlegel <adrian@liip.ch>
2011-07-07 11:55:11 +0200

    fixed tar generating script

--- git-arch.sh
+++ git-arch.sh
@@ -2,8 +2,7 @@
 TMP=$1
 NV=$2
 
-export NV TMP
-echo "NV: $NV / TMP: $TMP"
-git archive --format=tar --prefix="${NV}" --output="${TMP}/${NV}.tar" HEAD
+export NV TMP
+git archive --format=tar --prefix="${NV}/" --output="${TMP}/${NV}.tar" HEAD
 cd `git rev-parse --show-cdup`
-git submodule --quiet foreach 'FNAME=`mktemp -p /tmp`;git archive --format=tar --output=$FNAME --prefix=${path}/ $sha1;tar -Af "${TMP}/${NV}.tar" $FNAME;rm $FNAME'
+git submodule foreach 'FNAME=`mktemp -p /tmp rpmtarXXXXXX`;git archive --format=tar --output=$FNAME --prefix=${NV}/`basename ${path}`/ $sha1;tar -Af "${TMP}/${NV}.tar" $FNAME;rm $FNAME'
